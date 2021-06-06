5f57243bf13199871f74863db8b625453c3803d3
Oscar Calllisaya <oreynaldo.cl@gmail.com>
2021-06-06 19:58:00 -0400

    Queue solution in JS.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Testing');
+const renderGraph = (graph) => {
+  let currentQueue = [graph];
+  let newQueue = [];
+  const resultQueue = [];
+  while(currentQueue.length > 0) {
+    newQueue = [];
+
+    while (currentQueue.length > 0) {
+      const item = currentQueue.shift();
+      resultQueue.push(item.text);
+      if (item.left) {
+        newQueue.push(item.left);
+      }
+      if (item.right) {
+        newQueue.push(item.right);
+      }
+    }
+    currentQueue = newQueue;
+  }
+  return resultQueue.join(',');
+}
+//       A
+//    B     C
+//   D     E F
+const graph = {
+  text: 'A',
+  left: {
+    text: 'B',
+    left: {
+      text: 'D'
+    },
+  },
+  right: {
+    text: 'C',
+    left: {
+      text: 'E'
+    },
+    right: {
+      text: 'F'
+    },
+  }
+}
+
+console.log(renderGraph(graph));
